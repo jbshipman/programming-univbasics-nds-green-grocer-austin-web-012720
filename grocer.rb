e38ad33e046869345862939d7a30127c7ad19145
James Shipman <jbshipman@icloud.com>
2020-01-08 19:38:04 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,20 +1,54 @@
 def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
+
+  i = 0 
+  while i < collection.length do 
+    if collection[i][:item] == name 
+      return collection[i]
+    end 
+    i += 1   
+  end 
+
 end
 
 def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
-end
+  cartSummary = []
+  i = 0 
+  
+  while i < cart.length do 
+    cartSummary_item = find_item_by_name_in_collection(cart[i][:item], cartSummary)
+    if cartSummary_item != nil 
+      cartSummary_item[:count] += 1 
+    else 
+      cartSummary_item = {
+        :item => cart[i][:item],
+        :price => cart[i][:price],
+        :clearance => cart[i][:clearance],
+        :count => 1 
+      }
+      cartSummary << cartSummary_item
+    end 
+    i += 1 
+  end 
+  cartSummary 
+end 
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  i = 0 
+  while i < cart.length do 
+    if (cart[i][:item] == coupons[i][:item]) && (cart[i][:count] >= coupons[i][:num])
+      new_count = coupons[i][:num] - cart[i][:count] 
+      cart = {
+        :item => "#{cart[i][:item]} W/COUPON",
+        :price => coupons[i][:cost] / coupons[i][:num],
+        :clearance => cart[i][:clearance],
+        :count => cart[i][:num]
+      }
+      cart[i][:count] = new_count 
+    end
+    i += 1 
+  end 
+
+  cart
 end
 
 def apply_clearance(cart)
